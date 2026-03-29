2ad918bef098b5d3f257f8957f2147dc807666f1
srijan <srijanpokhrel282@gmail.com>
2026-03-29 14:32:22 +0545

    multiply added

---
+++ multiply.py
@@ -0,0 +1,9 @@
+# Take two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Multiply the numbers
+result = num1 * num2
+
+# Display the result
+print("The multiplication result is:", result)
